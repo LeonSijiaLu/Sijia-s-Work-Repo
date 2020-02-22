3a8927ec4141c3f36e767f9d88dad58227249e82
LeonSijiaLu <leonsijialu1@gmail.com>
2020-02-21 19:29:32 -0500

    Add new folders

---
+++ Golang/Algorithm/Sorting/Insertion_Sort.go
@@ -0,0 +1,27 @@
+package program
+
+import "fmt"
+
+func InsertionSort(array []int) []int {
+	if len(array) == 1{
+		return array
+	}
+	counter := 1
+	for {
+		for i := counter; i > 0; i-- {
+			if array[i] < array[i-1] {
+				array[i-1], array[i] = array[i], array[i-1]
+			}
+		}
+		counter++
+		if counter == len(array) {
+			break
+		}
+	}
+	return array
+}
+
+func main(){
+	array := []int{123, 534, 213, 8768, 42, 213, 8560, 324, 756, 123, 987}
+	fmt.Println(InsertionSort(array))
+}
